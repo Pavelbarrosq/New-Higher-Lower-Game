56d781b368b8d63342664a9bf93ea20f33ed6cc4
Pavel Barros Quintanilla <pavelbarrosquintanilla@Pavels-MacBook-Pro.local>
2019-01-20 16:22:14 +0100

    update before score implement

--- Higher Lower Game/Controller/ViewController.swift
+++ Higher Lower Game/Controller/ViewController.swift
@@ -10,10 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
     let allNumbers = NumberBank()
-    let randomIndex = Int.random(in: 0...10)
+    let randomIndex = Int.random(in: 0...11)
     var nextNumberOnScreen: ScreenNumber!
     var currentNumberOnScreen: ScreenNumber!
     var nextNumber = 0
+    var skipNumberOnScreen: ScreenNumber!
+    
     
     var numberOnLabel = 0
     
@@ -23,86 +25,64 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
        
-        //let numberOnLabel =
-        currentNumberOnScreen = allNumbers.numberArray[randomIndex - 1]
-        screenLabel.text = "\((currentNumberOnScreen.displayNumber))"
-        nextNumberOnScreen = allNumbers.numberArray[Int.random(in: 0...10)]
-        
-        
-        
-        
-        
-        
-        
+        play()
         
     }
 
     @IBAction func lowerButtonPressed(_ sender: UIButton) {
-        
-        
-        
-        if nextNumberOnScreen.displayNumber < currentNumberOnScreen.displayNumber{
-            
+        if nextNumberOnScreen.displayNumber < currentNumberOnScreen.displayNumber {
             nextLabelNumber()
-            
         }
-//        else {
-//            
-//            startOver()
-//        }
-        
+        else if nextNumberOnScreen.displayNumber == currentNumberOnScreen.displayNumber{
+            nextLabelNumber()
+        }
+        else {
+            print("You loose, the next number was \(nextNumberOnScreen.displayNumber)")
+            play()
+            print("New Game, new number is \(currentNumberOnScreen.displayNumber)")
+        }
     }
     
     @IBAction func higherButtonPressed(_ sender: UIButton) {
-        
         if nextNumberOnScreen.displayNumber > currentNumberOnScreen.displayNumber {
-            
             nextLabelNumber()
-            
         }
-            
-//        else {
-//            startOver()
-//        }
-        
+        else if nextNumberOnScreen.displayNumber == currentNumberOnScreen.displayNumber{
+            nextLabelNumber()
+        }
+        else {
+            print("You loose, the next number was \(nextNumberOnScreen.displayNumber)")
+            play()
+            print("New Game, new number is \(currentNumberOnScreen.displayNumber)")
+        }
+    }
+    
+    func play() {
+        currentNumberOnScreen = allNumbers.numberArray[randomIndex + 1]
+        screenLabel.text = "\((currentNumberOnScreen.displayNumber))"
+        nextNumberOnScreen = allNumbers.numberArray[Int.random(in: 0...10)]
     }
     
     
-    
     func updateUI() {
-        
     }
 
     
     func nextLabelNumber() {
-        
         currentNumberOnScreen = nextNumberOnScreen
         screenLabel.text = "\((currentNumberOnScreen.displayNumber))"
         nextNumberOnScreen = allNumbers.numberArray[Int.random(in: 0...10)]
-        
-        
-        
-        
-//        nextNumber = nextNumberOnScreen.displayNumber
-//
-//        screenLabel.text = "\(nextNumber)"
-//
     }
     
-        
-    
+    func skipNextNumber() {
+    }
     
     func checkAnswer() {
-        
-        
     }
-    
     
     func startOver() {
-        
         nextLabelNumber()
     }
-    
 }
 
 //    func currentNumber() {
